c4d298e5dbbc1961c7c7925ba1921fe7a5643b9f
Carla <carlajoumier@gmail.com>
2021-08-05 00:34:34 -0300

    add footer

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ function toggleMenu() {
   let siteNav = document.getElementById("site-nav");
   siteNav.classList.toggle("nav-open");
 }
+
+const isVisible = "is-visible";
+
+document.addEventListener("click", (e) => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
